d0ed935dc21ece4fc095ba4019ab42a6f1d5a890
liuhaikuan <liuhaikuan@game2sky.com>
2019-07-12 19:29:31 +0800

    修改插入算法内循环

--- algorithm/ide/src/com/lhk/algs4/Insertion.java
+++ algorithm/ide/src/com/lhk/algs4/Insertion.java
@@ -6,20 +6,19 @@ import edu.princeton.cs.algs4.In;
 
 public class Insertion extends SortTemplate {
     @Override
-    public void sort(Comparable[] a){
+    public void sort(Comparable[] a) {
         int N = a.length;
-        for(int i = 1; i < N; i++){
-            for(int j = i; j > 0; j--){
-                if (less(a[i], a[i-j])) {
-                    exch(a, i, i - j);
-                }
+        for (int i = 1; i < N; i++) {
+            for (int j = i; j > 0 && less(a[i], a[j - 1]); j--) {
+                exch(a, i, i - j);
             }
         }
     }
 
 
     public static void main (String[] args){
-        String[] a = In.readStrings();
+        In in = new In();
+        String[] a = in.readAllStrings();
         Insertion insertion = new Insertion();
         insertion.show(a);
         insertion.sort(a);
